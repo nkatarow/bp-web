8532f11765a7a2c443746a851c5369b8d49f0c0f
Nick Katarow <nick@turnstylestudio.com>
2016-07-28 13:55:28 -0700

    craft update

--- craft/app/vendor/composer/autoload_static.php
+++ craft/app/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInita0fd21e9ffef9e7c1a46f85a3bf2e24f
+class ComposerStaticInitb075020d56df7d35110db534e9d40947
 {
     public static $prefixLengthsPsr4 = array (
         'l' => 
@@ -666,10 +666,10 @@ class ComposerStaticInita0fd21e9ffef9e7c1a46f85a3bf2e24f
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInita0fd21e9ffef9e7c1a46f85a3bf2e24f::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInita0fd21e9ffef9e7c1a46f85a3bf2e24f::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInita0fd21e9ffef9e7c1a46f85a3bf2e24f::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInita0fd21e9ffef9e7c1a46f85a3bf2e24f::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitb075020d56df7d35110db534e9d40947::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitb075020d56df7d35110db534e9d40947::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitb075020d56df7d35110db534e9d40947::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInitb075020d56df7d35110db534e9d40947::$classMap;
 
         }, null, ClassLoader::class);
     }
